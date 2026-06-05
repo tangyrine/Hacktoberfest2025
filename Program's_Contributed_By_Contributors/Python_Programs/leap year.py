6fa12f64945e967928e3f71cac8f27ca201ca85f
suksome <sakshamsai26sep@gmail.com>
2026-06-05 08:03:14 +0200

    Modified hello_world.py (#3009)

---
+++ Program's_Contributed_By_Contributors/Python_Programs/leap year.py
@@ -0,0 +1,13 @@
+# Check leap year
+
+def leap_year_check(yr):
+    leap = False
+    if yr % 4 == 0:
+        leap = True
+        if yr % 100 ==0 and yr % 400 != 0:
+            leap = False
+        else: 
+            leap = True
+        print(leap?"{0} is a leap year".format(yr):"{0} is not a leap year".format(yr))
+
+leap_year_check(1700)
