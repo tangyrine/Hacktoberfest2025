26d47d9e015352fe9e690a0e43d503f248e8a7da
shivam-sharma-03 <shivamsharma2022@jklu.edu.in>
2026-06-05 03:17:42 +0200

    feat(Tic Tac Toe): created a tic tac toe game using Htlm Css Js. (#5818)

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2035,8 +2035,8 @@ contributors = [
   },
   {
     id: 420,
-    fullname: "Auro S.",
-    username: "https://github.com/aurocodes",
+    fullname: "Keshav R Karanth",
+    username: "https://github.com/keshavrkaranth",
   },
 
   {
@@ -2059,9 +2059,14 @@ contributors = [
     fullname: "Sumit Br",
     username: "https://github.com/SumitBr",
   },
-    {
+  {
     id: 425,
-    fullname: "Devender Pal",
-    username: "https://github.com/devender15",
+    fullname: "Auro S.",
+    username: "https://github.com/aurocodes",
+  },
+  {
+    id: 425,
+    fullname: "Shivam Sharma",
+    username: "https://github.com/shivam-sharma-03",
   },
 ];
